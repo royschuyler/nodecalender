6c6441352b7b372e689bcb7c57c6d404588b88e4
royschuyler <royschuyler@yahoo.com>
2015-07-24 10:17:42 -0500

    updated presented year to fix the jan/feb year -1

--- input.js
+++ input.js
@@ -1,22 +1,23 @@
 var argv = require('yargs')
+
 var monthOfYear;
-var moy = process.argv[2].toLowerCase().charAt(0).toUpperCase() + process.argv[2].slice(1);
+var moy;
 var monthOfYear = process.argv[2].toLowerCase().charAt(0).toUpperCase() + process.argv[2].slice(1);
 var dom = process.argv[3];
 var y = process.argv[4];
 
-if (moy == 'January')   { moy = 13; y = process.argv[4] - 1 }
-if (moy == 'Februrary') { moy = 14; y = process.argv[4] - 1 }
-if (moy == 'March')     { moy = 3 }
-if (moy == 'April')     { moy = 4 }
-if (moy == 'May')       { moy = 5 }
-if (moy == 'June')      { moy = 6 }
-if (moy == 'July')      { moy = 7 }
-if (moy == 'August')    { moy = 8 }
-if (moy == 'September') { moy = 9 }
-if (moy == 'October')   { moy = 10 }
-if (moy == 'November')  { moy = 11 }
-if (moy == 'December')  { moy = 12 }
+if (monthOfYear == 'January')   { moy = 13; y = process.argv[4] - 1 }
+if (monthOfYear == 'February') { moy = 14; y = process.argv[4] - 1 }
+if (monthOfYear == 'March')     { moy = 3 }
+if (monthOfYear == 'April')     { moy = 4 }
+if (monthOfYear == 'May')       { moy = 5 }
+if (monthOfYear == 'June')      { moy = 6 }
+if (monthOfYear == 'July')      { moy = 7 }
+if (monthOfYear == 'August')    { moy = 8 }
+if (monthOfYear == 'September') { moy = 9 }
+if (monthOfYear == 'October')   { moy = 10 }
+if (monthOfYear == 'November')  { moy = 11 }
+if (monthOfYear == 'December')  { moy = 12 }
 
 var dayOfWeek;
 var dayOfMonth = parseInt(dom);
@@ -25,7 +26,9 @@ var year = parseInt(y);
 var K = year % 100;
 var J = (year - K) / 100;
 
-var solve = (dayOfMonth + parseInt((13 * (moy + 1) / 5)) + K + parseInt(K / 4) + parseInt(J / 4) + 5 * J) % 7;
+// console.log(moy)
+
+var solve = (dayOfMonth + parseInt((13 * (month + 1) / 5)) + K + parseInt(K / 4) + parseInt(J / 4) + 5 * J) % 7;
 
 if (solve == 0) { dayOfWeek = 'Saturday' };
 if (solve == 1) { dayOfWeek = 'Sunday' };
@@ -35,14 +38,19 @@ if (solve == 4) { dayOfWeek = 'Wednsday' };
 if (solve == 5) { dayOfWeek = 'Thursday' };
 if (solve == 6) { dayOfWeek = 'Friday' };
 
-// console.log(dayOfWeek);
+console.log(dayOfWeek);
 
 
 //******************************CALENDER*********************************************************************
 
 //6 lines -- 42 spots
+var fixedYear;
+
+if (monthOfYear == 'January' || monthOfYear == 'February') { fixedYear = y + 1 }
+  else { fixedYear = y };
+
 var spot;
-var presentMonth = '     ' + monthOfYear + ' ' + y;
+var presentMonth = '     ' + monthOfYear + ' ' + fixedYear;
 var presentDays = 'Su Mo Tu We Th Fr Sa';
 
 
